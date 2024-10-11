727bd8edd148e187d96832fa47e876e5bd78dbb9
Guilherme Schmidt <guischmidt201@gmail.com>
2024-10-11 14:15:41 -0300

    organizacao das pastas

---
+++ Back-End/src/test/java/com/sentimento/lexicos/LexicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentimento.lexicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
