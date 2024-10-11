f5390ebc0ff6d0d0968cf60fec92ee36be786c81
Guilherme Schmidt <guischmidt201@gmail.com>
2024-10-11 14:15:00 -0300

    organizacao das pastas

--- src/main/java/com/sentimento/lexicos/repositories/LexicalTermRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sentimento.lexicos.repositories;
-
-import com.sentimento.lexicos.domain.LexicalTerm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LexicalTermRepository extends JpaRepository<LexicalTerm, Long> {
-}
--- src/main/java/com/sentimento/lexicos/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sentimento.lexicos.repositories;
-
-import com.sentimento.lexicos.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
